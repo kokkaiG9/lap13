f8fa8581d0e91a1d3c691d26244f8cc4056d0299
kokkaiG9 <gowsky.9.2548@gmail.com>
2024-12-27 16:31:58 +0700

    finish

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,32 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool gg [][M],int s,int x,int y){
+    for(int i = 0 ; i < 30 ; i++){
+        for(int j = 0; j < 70; j++){
+            if(sqrt(pow(i-x,2)+pow(j-y,2))<=s-1){
+                gg[i][j] = 1 ;
+            }
+        }    
+    }
+}
+
+void showImage(const bool gg [][M]){
+    cout << "------------------------------------------------------------------------" << endl;
+    for(int i = 0 ; i < 30 ; i++){
+        cout << "|";
+        for(int j = 0; j < 70; j++){
+            if(gg[i][j]==0){
+                cout << " ";
+            }else{
+                cout << "*";
+            }
+            
+        }
+        cout << "|" << endl;
+        
+    }
+    cout << "------------------------------------------------------------------------" <<endl;
+
+}
